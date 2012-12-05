826f7f36f918e0233549d682073698c81b5273c2
eriksk <erikskoglund88@gmail.com>
2012-12-05 09:19:04 +0100

    added junitexamples and ppt

--- src/se/hb/bank/exceptions/TransferNotAllowedException.java
+++
@@ -1,9 +0,0 @@
-package se.hb.bank.exceptions;
-
-public class TransferNotAllowedException extends Exception{
-
-    public TransferNotAllowedException(String message) {
-        super(message);
-    }
-    
-}
